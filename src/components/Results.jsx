eb9a35b24e131e63fdb5b04d13d34f55adbe372d
Tina <tina@Tinas-MacBook-Air.local>
2024-12-18 15:07:40 +0800

    Feat: Add Result component

---
+++ src/components/Results.jsx
@@ -0,0 +1,7 @@
+import { calculateInvestmentResults } from "../util/investment";
+
+export default function Results({ userInput }) {
+  const resultData = calculateInvestmentResults(userInput);
+  console.log(resultData);
+  return <p>Results...</p>;
+}
